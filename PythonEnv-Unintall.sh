5d1d005655bb0fa02f34c301be3aedd73bbd05a1
liamatkinsstorm <liam.atkins@storm-technologies.com>
2023-08-08 17:53:41 +0100

    Amended config and checked worked

--- PythonEnv-Unintall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ./.venv/
-rm ./Pipfile
-rm ./Pipfile.lock
--- PythonEnv-install.sh
+++ PythonEnv-install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-python3 -m ensurepip --upgrade
-python3 -m pip install --upgrade pip # use a virtual development environment
-pipenv install --python 3.11        # use Python 3.9 or later
-pipenv install -r requirements.txt  # install required Python packages
+#sudo apt update 
+#sudo apt upgrade
+#sudo apt -y install python3-pip
+#sudo apt -y install virtualenv
+#sudo apt install python3-venv
+
+python3 -m venv /home/liam/Documents/venv # Creates a directory named 'venv' in the current directory, which contains pip, interpreter, scripts, and libraries.
+source /home/liam/Documents/venv/bin/activate
+
+pip install -r /home/liam/GitRepo/ansible-ise-1/requirements.txt  # install required Python packages
+
+#deactivate
